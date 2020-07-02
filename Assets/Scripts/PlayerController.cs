707e1bece224ac6035b3206e7316fea5a5727a5f
dayakarprashanth <56269912+dayakarprashanth@users.noreply.github.com>
2020-07-02 12:40:17 -0500

    1.4 Allowing Players to access controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    public float _speed = 5.0f;
+    public float _turnspeed;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //We will move the vehicle forward
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime * forwardInput);
+        transform.Translate(Vector3.right * _turnspeed * Time.deltaTime * horizontalInput);
     }
 }
